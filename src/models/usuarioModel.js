38fad221e9808ef6d773d9c4dfa9962f6d41c44f
rodgmartins-dev <rodrigo.martins@sptech.school>
2025-10-25 19:10:51 -0300

    Commit

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -6,7 +6,7 @@ function autenticar(email, senha) {
     email,
     senha
   );
-  
+
   // Usando a tabela cadastro e incluindo o nome no SELECT
   var instrucaoSql = `
         SELECT id, nome, email FROM cadastro WHERE email = '${email}' AND senha = '${senha}';
@@ -17,14 +17,20 @@ function autenticar(email, senha) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, cnpj, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, cnpj, email, senha);
-    
-    // CORRIGIDO: mudei de 'usuario' para 'cadastro'
-    var instrucaoSql = `
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
+    nome,
+    cnpj,
+    email,
+    senha
+  );
+
+  // CORRIGIDO: mudei de 'usuario' para 'cadastro'
+  var instrucaoSql = `
         INSERT INTO cadastro (nome, cnpj, email, senha) VALUES ('${nome}', '${cnpj}', '${email}', '${senha}');
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 module.exports = {
