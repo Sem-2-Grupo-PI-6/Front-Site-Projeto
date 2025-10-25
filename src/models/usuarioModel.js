e976c6b77e85edcafe6ec7040854156b69ca4f96
rodgmartins-dev <rodrigo.martins@sptech.school>
2025-10-25 18:47:52 -0300

    Commit

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -8,7 +8,7 @@ function autenticar(email, senha, identCodigo) {
     identCodigo
   );
   var instrucaoSql = `
-        SELECT id, email, identCodigo FROM usuario WHERE email = '${email}' AND senha = '${senha}' AND identCodigo = '${identCodigo}';
+        SELECT id, email, codigo FROM usuarios WHERE email = '${email}' AND senha = '${senha}' AND codigo = '${identCodigo}';
     `;
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
@@ -16,15 +16,21 @@ function autenticar(email, senha, identCodigo) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, cnpj, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, cnpj, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
+    nome,
+    cnpj,
+    email,
+    senha
+  );
+
+  // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+  //  e na ordem de inserção dos dados.
+  var instrucaoSql = `
         INSERT INTO usuario (nome, cnpj, email, senha) VALUES ('${nome}', '${cnpj}', '${email}', '${senha}');
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 module.exports = {
